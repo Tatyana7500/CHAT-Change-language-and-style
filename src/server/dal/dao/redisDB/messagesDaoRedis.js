a6e49219156ef1042a4aa818a439405edd84a52e
marselenka <shkodina-le@yandex.ru>
2019-09-22 09:37:04 +0300

    CHAT-10-DAL Redis

--- src/server/dal/dao/redisDB/messagesDaoRedis.js
+++ src/server/dal/dao/redisDB/messagesDaoRedis.js
@@ -1,30 +1,73 @@
 const DAO = require('../dao');
 const config = require('../../../config');
+const redis = require('redis');
 const util = require('../util');
+const url = config.settings.redis.connectionRedis;
 
 function MessagesDaoRedisDB() {
-    this.connection = null;
-    this.model = null;
+    this.client = redis.createClient(url);
+    this.client.on('connection', function () {
+        console.log('Redis client connected');
+    });
+
+    this.client.on('error', function (err) {
+        console.log(err);
+    });
 }
 
 MessagesDaoRedisDB.prototype = Object.create(DAO.prototype);
 MessagesDaoRedisDB.prototype.constructor = MessagesDaoRedisDB;
 
 MessagesDaoRedisDB.prototype.initialize = function () {
-    if (this.connection) {
-        return;
-    }
-
-    const url = config.settings.redis.connectionString;
 };
 
-MessagesDaoRedisDB.prototype.create = async function (obj) {
+MessagesDaoRedisDB.prototype.create = async function (msg) {
+    await this.client.zadd('msg_' + msg.receiver, Date.now(), JSON.stringify(msg));
+    await this.client.zadd('msg_' + msg.sender + '_' + msg.receiver, Date.now(), JSON.stringify(msg));
 };
 
 MessagesDaoRedisDB.prototype.readByReceiver = async function (receiver) {
+    return new Promise((resolve, reject) => {
+        const msgs = [];
+        this.client.zrange('msg_' + receiver, 0, -1, function (err, string) {
+            if (err) {
+                reject(err);
+            }
+            for (let i = 0; i < string.length; i++) {
+                msgs.push(JSON.parse(string[i]));
+            }
+            resolve(msgs);
+        });
+    });
 };
 
 MessagesDaoRedisDB.prototype.readBySenderAndReceiver = async function (sender, receiver) {
+    return new Promise((resolve, reject) => {
+        const messages = [];
+        const th = this;
+        this.client.zrange('msg_' + sender + receiver, 0, -1, function (err, string) {
+            if (err) {
+                reject(err);
+            }
+            for (let i = 0; i < string.length; i++) {
+                messages.push(JSON.parse(string[i]));
+            }
+            let temp = sender;
+            sender = receiver;
+            receiver = temp;
+            th.client.zrange('msg_' + receiver + sender, 0, -1, function (err, string) {
+                if (err) {
+                    reject(err);
+                }
+                for (let i = 0; i < string.length; i++) {
+                    messages.push(JSON.parse(string[i]));
+                }
+                messages.sort(util.dynamicSort('date'));
+                resolve(messages);
+            });
+        });
+    });
 };
 
 module.exports = MessagesDaoRedisDB;
+
--- src/server/dal/dao/redisDB/usersDaoRedis.js
+++ src/server/dal/dao/redisDB/usersDaoRedis.js
@@ -1,32 +1,78 @@
 const DAO = require('../dao');
 const config = require('../../../config');
+const redis = require('redis');
+const url = config.settings.redis.connectionRedis;
 
 function UsersDaoRedisDB() {
-    this.connection = null;
-    this.client = null;
+    this.client = redis.createClient(url);
+    this.client.on('connection', function () {
+        console.log('Redis client connected');
+    });
+
+    this.client.on('error', function (err) {
+        console.log(err);
+    });
 }
 
 UsersDaoRedisDB.prototype = Object.create(DAO.prototype);
 UsersDaoRedisDB.prototype.constructor = UsersDaoRedisDB;
-
 UsersDaoRedisDB.prototype.initialize = function () {
-    if (this.connection) {
-        return;
-    }
-
-    const url = config.settings.redis.connectionString;
 };
 
-UsersDaoRedisDB.prototype.create = async function (obj) {
+UsersDaoRedisDB.prototype.create = async function (object) {
+    const user = {
+        _id: object.email,
+        name: object.name,
+        email: object.email,
+        password: object.password,
+    };
+    await this.client.set('user_' + user.email, JSON.stringify(user));
 };
 
-UsersDaoRedisDB.prototype.readUser = async function(email, password) {
+UsersDaoRedisDB.prototype.readUser = async function (email, password) {
+    return new Promise((resolve, reject) => {
+        const key = 'user_' + email;
+        this.client.get(key, function (err, string) {
+            if (err) {
+                reject(err);
+            }
+            let user = JSON.parse(string);
+            if (user.email === email && user.password === password) {
+                resolve(user);
+            }
+        });
+    });
 };
 
 UsersDaoRedisDB.prototype.readAll = async function () {
+    const th = this;
+    return new Promise((resolve, reject) => {
+        this.client.scan('0', 'MATCH', 'user_*', 'COUNT', '1000', function (err, keys) {
+            if (err) {
+                reject(err);
+            }
+            th.client.mget(keys[1], function (err, string) {
+                const users = [];
+                for (let i = 0; i < string.length; i++) {
+                    users.push(JSON.parse(string[i]));
+                }
+                resolve(users);
+            });
+        });
+    });
 };
 
-UsersDaoRedisDB.prototype.readUserToId = async function(id) {
+UsersDaoRedisDB.prototype.readUserToId = async function (id) {
+    const key = 'user_' + id;
+    return new Promise((resolve, reject) => {
+        this.client.get(key, function (err, string) {
+            if (err) {
+                reject(err);
+            }
+            let user = JSON.parse(string);
+            resolve([user]);
+        });
+    });
 };
 
 module.exports = UsersDaoRedisDB;
