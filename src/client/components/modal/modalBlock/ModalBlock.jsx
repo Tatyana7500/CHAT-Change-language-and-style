9739314b9f2065b0b906e260858d9870efa73d94
Your Name <you@example.com>
2019-09-24 21:29:19 +0300

    add modal window

--- src/client/components/modal/modalBlock/ModalBlock.jsx
+++ src/client/components/modal/modalBlock/ModalBlock.jsx
@@ -2,27 +2,41 @@ import React, { Fragment } from 'react';
 import PropTypes from 'prop-types';
 import SettingTheme from '../../common/themeDropdown/ThemeDropdown';
 import SettingLanguage from '../../common/languageDropdown/LanguageDropdown';
-import ModalWrapper from '../modalWrapper/ModalWrapper.jsx';
 
 const ModalBlock = (props) => {
     const { translate, defaultCountry, changeTheme, changeLanguage, theme, handleHide } = props;
 
     return (
         <Fragment>
-            <ModalWrapper/>
+            <div className='model-wrapper'>
+            </div>
             <div className='model'>
                 <button className='settings close' onClick={handleHide}>
                     <img src='src/client/assets/icons/close.png' width='20' height='20' />
                 </button>
-                <div className='model__settings_1'>
-                    <SettingTheme
-                        theme={theme}
-                        changeTheme={changeTheme}
-                    />
-                    <SettingLanguage
-                        defaultCountry={defaultCountry}
-                        changeLanguage={changeLanguage}
-                    />
+                <div className='model__settings'>
+                    <div className='model__settings-theme'>
+                        <p>{translate('theme')}</p>
+                        <SettingTheme
+                            theme={theme}
+                            changeTheme={changeTheme}
+                        />
+                    </div>
+                    <div className='model__settings-language'>
+                        <p>{translate('language')}</p>
+                        <SettingLanguage
+                            defaultCountry={defaultCountry}
+                            changeLanguage={changeLanguage}
+                        />
+                    </div>
+                </div>
+                <div className='model__settings'>
+                    <div className='model__settings-emoji'>
+                        <p>{translate('emoji')}</p>
+                    </div>
+                    <div className='model__settings-private'>
+                        <p>{translate('chat')}</p>
+                    </div>
                 </div>
             </div>
         </Fragment>
